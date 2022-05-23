6e9c461097244fa1e87ea6d8b0356a13e7aed29b
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-23 12:38:22 -0400

    create and display 16 test divs

---
+++ main.js
@@ -0,0 +1,11 @@
+let createDiv = document.createElement('div');
+let createP = document.createElement('p');
+
+
+
+for (let i=1; i<17; i+=1){
+    createP.innerHTML+=i+'testing<br>';
+    document.querySelector('.container').appendChild(createDiv);
+    document.querySelector('.container').appendChild(createP); 
+
+}
