fcedbf439bfe1d017e62c4639ee8022b0c0e2cff
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-25 12:47:05 -0400

    fixed type and added alert msg

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ canvasButton.addEventListener("click", (e) => {
 
 function newGrid() {
     userNum = prompt('Please enter the new size of the canvas.\n(e.g. enter 4 = 4x4 square grid)\nMAX: 100');
-    if (useNum === 'sugma') {
-        document.body.innerHTML+='halow';
+    if (userNum === parseInt(data, 10)) {
+        alert("Sorry, that is not a number. Enter a NUMBER (1-100)");
     }
     else{
-        return
+        alert("Good job *gives head pats*");
     }
 }
     
