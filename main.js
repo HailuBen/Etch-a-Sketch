81e60198bf056c4e849f7270be5c42592960c7ad
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-25 12:03:39 -0400

    update prompt text

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ canvasButton.addEventListener("click", (e) => {
 })
 
 function newGrid() {
-    userMsg = prompt('sugma');
+    userMsg = prompt('Please enter the new size of the canvas.\n(e.g. 4x4 = 4,4)');
     if (userMsg === 'sugma') {
         document.body.innerHTML+='halow';
     }
