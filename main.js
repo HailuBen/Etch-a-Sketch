3960cac8cdf1f2621425558e9db6d0b635505e2b
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-25 09:44:43 -0400

    Update main.js -Update for loop to input 32 divs. -Add gridDiv class to createDiv -Change .container to .grid-container -Comment out createDiv and for loop -Create const container to access container id -Create function makeRows

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-let createDiv = document.createElement('div');
+// let createDiv = document.createElement('div');
+// createDiv.classList.add('gridDiv');
 let createP = document.createElement('p');
+createP.classList.add('gridDiv');
+const container = document.getElementById("container");
 
-for (let i=1; i<17; i+=1){
-    createP.innerHTML+=i+'testing<br>';
-    document.querySelector('.container').appendChild(createDiv);
-    document.querySelector('.container').appendChild(createP); 
-}
+// for (let i=1; i<33; i+=1){
+//     createP.innerHTML+=i+'testing<br>';
+//     document.querySelector('.grid-container').appendChild(createDiv);
+//     document.querySelector('.grid-container').appendChild(createP);
+// }
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (c = 0; c < (rows * cols); c++) {
+        let cellDiv = document.createElement("div");
+        cellDiv.innerText = (c + 1);
+        container.appendChild(cellDiv).className = "gridDiv";
+    };
+};
+    
+makeRows(16, 16);
