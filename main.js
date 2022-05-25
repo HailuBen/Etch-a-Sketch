bf1d3d95fb8c185ae5b909581b8b1ef160878cfa
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-25 11:48:31 -0400

    Update main.js -Delete comments -Add event listener for button click -Delete createP -Add variables -Change container to gridContainer

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-// let createDiv = document.createElement('div');
-// createDiv.classList.add('gridDiv');
-let createP = document.createElement('p');
-createP.classList.add('gridDiv');
-const container = document.getElementById("container");
-
-// for (let i=1; i<33; i+=1){
-//     createP.innerHTML+=i+'testing<br>';
-//     document.querySelector('.grid-container').appendChild(createDiv);
-//     document.querySelector('.grid-container').appendChild(createP);
-// }
+const gridContainer = document.getElementById("gridContainer");
+const canvasButton = document.getElementById("popup");
+let newCanvas;
+let newCanvasSize;
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
 
     for (c = 0; c < (rows * cols); c++) {
         let cellDiv = document.createElement("div");
-        // cellDiv.innerText = (c + 1);
-        container.appendChild(cellDiv).className = "gridDiv";
+        gridContainer.appendChild(cellDiv).className = "gridDiv";
     };
 };
 
+canvasButton.addEventListener("click", (e) => {
+    newCanvas=e.target.id;
+    newGrid();
+})
+
 function newGrid() {
-    
+    userMsg = prompt('sugma');
+    if (userMsg === 'sugma') {
+        document.body.innerHTML+='halow';
+    }
+    else{
+        return
+    }
 }
     
 makeRows(16, 16);
