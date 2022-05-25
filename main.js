72b027c47fe222ec6e342ebdc2ea4f5534127652
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-25 12:43:41 -0400

    change userMsg to userNum

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let newCanvasSize;
 function makeRows(rows, cols) {
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
-
     for (c = 0; c < (rows * cols); c++) {
         let cellDiv = document.createElement("div");
         gridContainer.appendChild(cellDiv).className = "gridDiv";
@@ -19,8 +18,8 @@ canvasButton.addEventListener("click", (e) => {
 })
 
 function newGrid() {
-    userMsg = prompt('Please enter the new size of the canvas.\n(e.g. enter 4 = 4x4 square grid)\nMAX: 100');
-    if (userMsg === 'sugma') {
+    userNum = prompt('Please enter the new size of the canvas.\n(e.g. enter 4 = 4x4 square grid)\nMAX: 100');
+    if (useNum === 'sugma') {
         document.body.innerHTML+='halow';
     }
     else{
