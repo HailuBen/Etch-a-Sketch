4661bc2df0d9bceb93a7124c0a38a7d35e893e50
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-26 15:59:31 -0400

    adjusted spacing and indent

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 function setUpEvents(){
 
+    
 
-function makeRows(rows, cols) {
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cellDiv = document.createElement("div");
-        gridContainer.appendChild(cellDiv).className = "gridDiv";
+    function makeRows(rows, cols) {
+        gridContainer.style.setProperty('--grid-rows', rows);
+        gridContainer.style.setProperty('--grid-cols', cols);
+        for (c = 0; c < (rows * cols); c++) {
+            let cellDiv = document.createElement("div");
+            gridContainer.appendChild(cellDiv).className = "gridDiv";
+        };
     };
-};
 
 
-makeRows(16, 16);
-}
+    makeRows(16, 16);
+    }
 window.onload = function(){
     setUpEvents();
 };
