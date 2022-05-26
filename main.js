c4607fbf9f7b23a54545b844b3f46fabba0bc628
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-26 17:00:56 -0400

    add newGrid function, click event to resize button -newGrid function now uses newGridNum to resize gridDivs

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 function setUpEvents(){
 
-    const gridContainer = document.getElementById("gridContainer");
+    const gridContainer = document.getElementById('gridContainer');
+    const resizeButton = document.getElementById('resize');
+    let newGridNum = 32;
 
     function makeGrid(rows, cols) {
         gridContainer.style.setProperty('--grid-rows', rows);
         gridContainer.style.setProperty('--grid-cols', cols);
         for (c = 0; c < (rows * cols); c++) {
-            let cellDiv = document.createElement("div");
-            gridContainer.appendChild(cellDiv).className = "gridDiv";
+            let cellDiv = document.createElement('div');
+            gridContainer.appendChild(cellDiv).className = 'gridDiv';
         };
     };
 
+    resizeButton.addEventListener('click', (e)=> {
+        newGrid();
+    })
+
+    function newGrid(rows, cols) {
+        rols = newGridNum;
+        cols = newGridNum;
+        gridContainer.style.setProperty('--grid-rows', newGridNum);
+        gridContainer.style.setProperty('--grid-cols', newGridNum);
+        for (c = 0; c < (newGridNum * newGridNum); c++) {
+            let cellDiv = document.createElement('div');
+            gridContainer.appendChild(cellDiv).className = 'gridDiv';
+        };
+    };
 
     makeGrid(16, 16);
     }
