6811a93e68cdd9ab1a1413e2f917bf443cb80cd9
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-26 19:51:51 -0400

    update makeGrid function, remove others

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
 function setUpEvents(){
 
     const gridContainer = document.getElementById('gridContainer');
-    const resizeButton = document.getElementById('resize');
-    let newGridNum = 32;
+    const specialBtn = document.getElementById('special');
+    const eraseBtn = document.getElementById('erase');
+    const resetBtn = document.getElementById('reset');
+    let slider = document.getElementById('resize');
+    let defaultColor = ('black');
 
-    function makeGrid(rows, cols) {
-        gridContainer.style.setProperty('--grid-rows', rows);
-        gridContainer.style.setProperty('--grid-cols', cols);
-        for (c = 0; c < (rows * cols); c++) {
-            let cellDiv = document.createElement('div');
-            gridContainer.appendChild(cellDiv).className = 'gridDiv';
-        };
+    function makeGrid(gridNum) {
+        let gridArea = gridNum*gridNum;
+        for (i = 0; i <= gridArea; i++) {
+            let gridItem = document.createElement('div');
+            gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+            gridContainer.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+            gridContainer.appendChild(cellDiv).className='gridDiv';
+        }
     };
 
-    resizeButton.addEventListener('click', (e)=> {
-        newGrid();
-    })
-
-    function newGrid(rows, cols) {
-        rols = newGridNum;
-        cols = newGridNum;
-        gridContainer.style.setProperty('--grid-rows', newGridNum);
-        gridContainer.style.setProperty('--grid-cols', newGridNum);
-        for (c = 0; c < (newGridNum * newGridNum); c++) {
-            let cellDiv = document.createElement('div');
-            gridContainer.appendChild(cellDiv).className = 'gridDiv';
-        };
-    };
-
-    makeGrid(16, 16);
     }
 window.onload = function(){
     setUpEvents();
