c574371b1e2d93c4184c95d7cfca9cbda3edcdc1
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-27 17:26:11 -0400

    recreate makeGrid function using slider value

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ function setUpEvents(){
     const eraseBtn = document.getElementById('erase');
     const resetBtn = document.getElementById('reset');
     let slider = document.getElementById('resize');
-    let defaultColor = ('black');
+    let gridNum = Number(slider.value);
 
     function makeGrid(gridNum) {
-        let gridArea = gridNum*gridNum;
-        for (i = 0; i <= gridArea; i++) {
+        let gridArea = (gridNum * gridNum);
+        for (let i = 0; i<gridArea; i++) {
             let gridItem = document.createElement('div');
-            gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
-            gridContainer.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
-            gridContainer.appendChild(cellDiv).className='gridDiv';
+            gridItem.id=i;
+            gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+            gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+            gridItem.classList.add('gridItem');
+            gridContainer.appendChild(gridItem);
         }
     };
+    makeGrid(gridNum);
 
     }
 window.onload = function(){
