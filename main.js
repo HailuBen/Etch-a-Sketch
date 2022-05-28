80bdcf5a90e7dfbc24250b66f4b75b12974014df
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-27 22:37:30 -0400

    fix slider functionality, add new functions -add arrow function for setting grid size -add arrow function for resizing grid, display new size -add start of draw function -update resizeGrid function with new functions -add start of resetGrid function -add event listener to grid item for draw

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
-function setUpEvents(){
-
+    const defaultGridSize = 16;
+    let currentGridSize = defaultGridSize;
     const gridContainer = document.getElementById('gridContainer');
     const specialBtn = document.getElementById('special');
     const eraseBtn = document.getElementById('erase');
     const resetBtn = document.getElementById('reset');
     let slider = document.getElementById('resize');
-    let gridNum = Number(slider.value);
+    let displaySliderSize = document.getElementById('sliderSize');
 
-    function makeGrid(gridNum) {
-        let gridArea = (gridNum * gridNum);
-        for (let i = 0; i<gridArea; i++) {
+    slider.onmousemove = (e) => displaySliderSize.innerHTML=(e.target.value)+'x'+(e.target.value)
+    slider.onchange = (e) => resizeGrid(e.target.value)
+    
+    let mouseDown = false
+    document.body.onmousedown = () => (mouseDown = true)
+    document.body.onmouseup = () => (mouseDown = false)
+
+    let setGridSize = (newGridSize) => currentGridSize = newGridSize;
+
+    function makeGrid(gridSize) {
+        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+        for (let i = 0; i<gridSize*gridSize; i++) {
             let gridItem = document.createElement('div');
             gridItem.id=i;
             gridItem.classList.add('gridItem');
-            gridContainer.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
-            gridContainer.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+            gridItem.addEventListener('mouseover', draw)
+            gridItem.addEventListener('mousedown', draw)
             gridContainer.appendChild(gridItem);
         }
     };
-    makeGrid(gridNum);
 
+    function draw(e) {
+        if (e.type === 'mouseover' && !mouseDown) return
+        
     }
+
+    // slider.addEventListener('beforeinput', function(){
+    //     // if statement for if there is any art on grid first
+    //     alert('WARNING: Your art will be lost if you resize.');
+    // })
+    function resizeGrid(value){
+        setGridSize(value);
+        resetGrid();
+    }
+
+    function resetGrid(){
+        gridContainer.innerHTML= '';
+        makeGrid(currentGridSize);
+    }
+
 window.onload = function(){
-    setUpEvents();
+    makeGrid(defaultGridSize);
 };
