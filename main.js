76c49f434817995cbdcc287bd12dbe9c1787616b
Ben Hailu <b.hailu.pk@gmail.com>
2022-05-30 15:14:18 -0400

    remove erase function and remnants

--- main.js
+++ main.js
@@ -14,47 +14,26 @@
         resetGrid();
     })
     
-    let mouseDown = false
-    document.body.onmousedown = () => (mouseDown = true)
-    document.body.onmouseup = () => (mouseDown = false)
-
     let setGridSize = (newGridSize) => currentGridSize = newGridSize;
 
     function makeGrid(gridSize) {
         gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
         for (let i = 0; i<gridSize*gridSize; i++) {
             gridItem = document.createElement('div');
             gridItem.id=i;
             gridItem.classList.add('gridItem');
-            gridItem.addEventListener('mouseover', draw)
-            gridItem.addEventListener('mousedown', draw)
             gridContainer.appendChild(gridItem);
-        }
+        }   
     };
 
-    function draw(e) {
-        if (e.type === 'mouseover' && !mouseDown) return
-        eraseGrid();
-    }
-
-    // slider.addEventListener('beforeinput', function(){
-    //     // if statement for if there is any art on grid first
-    //     alert('WARNING: Your art will be lost if you resize.');
-    // })
     function resizeGrid(value){
         setGridSize(value);
         resetGrid();
     }
-
     function resetGrid(){
         gridContainer.innerHTML= '';
         makeGrid(currentGridSize);
-    }
-
-    function eraseGrid(){
-        
     }
 
 window.onload = function(){
